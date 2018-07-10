5dc3145940ce671072f7cb86ce60e789fa28d4ca
David Pickart <davidstoreypickart@gmail.com>
2018-07-10 17:18:47 -0500

    Allow constant value argument for setter

--- src/setState.js
+++ src/setState.js
@@ -1,3 +1,4 @@
+import { isFunction } from 'lodash'
 import { set, get } from 'lodash/fp'
 
 /**
@@ -8,7 +9,7 @@ import { set, get } from 'lodash/fp'
  * @name setState
  * @type Function
  * @param {String} path - Path to the part of the state that will be set
- * @param {Function} transform - A function with arguments `(action, state, slice)` that can be used to transform the value that will be set. `slice` is the data, if any, that already exists in the state at `path`. The default transform function simply returns the action's payload.
+ * @param {Function} transform - A function with arguments `(action, state, slice)` that can be used to transform the value that will be set. `slice` is the data, if any, that already exists in the state at `path`. The default transform function simply returns the action's payload. If the same value is always set, it can be passed in place of the transform function.
  * @returns {Function} - A function that can be used in a reducer to handle an action.
  *
  * @example
@@ -24,6 +25,7 @@ import { set, get } from 'lodash/fp'
  *   [setCount]: setState('count'),
  *   [setCountInverse]: setState('count', action => action.payload * -1)
  *   [doubleExistingCount]: setState('count', (action, state, count) => count * 2)
+ *   [resetCount]: setState('count', 0)
  * })
  *
 **/
@@ -35,7 +37,8 @@ function defaultTransform (action) {
 function setState (path, transform=defaultTransform) {
   return function reducer (state, action) {
     const slice = get(path, state)
-    return set(path, transform(action, state, slice), state)
+    const value = isFunction(transform) ? transform(action, state, slice) : transform
+    return set(path, value, state)
   }
 }
 
